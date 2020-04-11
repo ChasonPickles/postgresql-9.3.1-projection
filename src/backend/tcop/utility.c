56aa82435b85942e3d29cf65547fd2ef955a089d
hbindra <hbindra@uwaterloo.ca>
2020-04-11 17:59:21 -0400

    add comments to utility.c

--- src/backend/tcop/utility.c
+++ src/backend/tcop/utility.c
@@ -63,6 +63,10 @@
 #include "utils/acl.h"
 #include "utils/guc.h"
 #include "utils/syscache.h"
+#include "utils/builtins.h"
+//#include "nodes/pg_list.h"
+//#include "utils/relcache.h"
+//#include "utils/rel.h"
 
 
 /* Hook for plugins to get control in ProcessUtility() */
@@ -927,16 +931,48 @@ ProcessUtilitySlow(Node *parsetree,
 					foreach(l, stmts)
 					{
 						Node	   *stmt = (Node *) lfirst(l);
+                        Relation rel;
+
+                        /* CS448 
+                            bool hasProjection;
+                            char* projectionName;
+                            ListCell* lc;
+                            Constraint * constraint;
+                        */
 
 						if (IsA(stmt, CreateStmt))
 						{
 							Datum		toast_options;
 							static char *validnsps[] = HEAP_RELOPT_NAMESPACES;
 
+
+                            /* CS448        
+                            foreach(lc, ((CreateStmt*) stmt)->projconstraints){
+                                constraint = lfirst(lc);
+                                if (constraint->contype == CONSTR_PROJECTION){
+                                    hasProjection = true;
+                                    projectionName = queryString;
+                                }
+                            }
+                            */
+
+                            //((Value*) ((Constraint*) ((CreateStmt*) stmt)->projconstraints->head->data->ptr_value)->keys->head->data->ptr_value)
+
+
 							/* Create the table itself */
 							relOid = DefineRelation((CreateStmt *) stmt,
 													RELKIND_RELATION,
 													InvalidOid);
+
+                            /* CS448
+                            if(hasProjection){
+                                rel = relation_open(relOid, AccessExclusiveLock);
+                                rel->hasProjections = true;
+                                rel->projections = list_make1(projectionName);
+                                //rel->rd_rel->hasProjection = true;
+                                relation_close(rel, AccessExclusiveLock);
+                            }
+                            */
 
 							/*
 							 * Let AlterTableCreateToastTable decide if this
